434568280f4eb6973c6446a0bd0da3031a5dc626
Omar Diab <o.s.diab@gmail.com>
2013-05-06 02:38:38 -0700

    did slideshow

--- assignments/1-accordian/js/slideshow.js
+++ assignments/1-accordian/js/slideshow.js
@@ -1,7 +1,33 @@
 var createSlideshow = function(slideshowElem, duration) {
-	// TODO: Implement createSlideshow.
+  // default value of duration
+  if (typeof duration === 'undefined') {
+    duration = 5000;
+  }
+
+  var fadeTime = 1000;
+
+  // initialization
+  slideshowElem.children().hide();
+  slideshowElem.children(':first').fadeIn(fadeTime);
+
+  // timer loop to produce slideshow
+  setInterval(function() {
+    // only visible child is the current element of the slideshow
+    cur = slideshowElem.children().filter(':visible');
+
+    // loop around when reached end
+    if (cur.is(':last-child')) {
+      next = slideshowElem.children(':first');
+    } else {
+      next = cur.next();
+    }
+
+    // animate
+    cur.fadeOut(fadeTime);
+    next.fadeIn(fadeTime);
+  }, duration);
 };
 
 $(document).ready(function() {
-	// TODO: Use createSlideshow to create a slideshow on the page.
+  createSlideshow($('.slideshow'));
 });
