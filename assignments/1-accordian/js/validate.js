c930312631c2afcf2d9f3f2e1ddfbb953e4ca01e
Omar Diab <o.s.diab@gmail.com>
2013-05-06 17:08:28 -0700

    validation done

--- assignments/1-accordian/js/validate.js
+++ assignments/1-accordian/js/validate.js
@@ -1,7 +1,67 @@
 var validateField = function(fieldElem, infoMessage, validateFn) {
-	// TODO: Implement validateField.
+  if (fieldElem.is(':last-child')) {
+    fieldElem.parent().append("<span></span>");
+  }
+  var formStatus = fieldElem.next();
+  var inputValue = fieldElem.val();
+
+  if (fieldElem.is(':focus')) {
+    formStatus.text(infoMessage);
+    formStatus.removeClass();
+    formStatus.addClass("info");
+    return;
+  }
+
+  if (!inputValue) {
+    formStatus.text("");
+    formStatus.removeClass();
+  } else if (validateFn(fieldElem.val())) {
+    formStatus.text("OK");
+    formStatus.removeClass();
+    formStatus.addClass("ok");
+  } else {
+    formStatus.text("Error");
+    formStatus.removeClass();
+    formStatus.addClass("error");
+  }
 };
 
 $(document).ready(function() {
-	// TODO: Use validateField to validate form fields on the page.
+  var createFormHandler = function(field, text, method) {
+    return function() {
+      validateField(field, text, method);
+    }
+  }
+
+  var userHandler = createFormHandler(
+    $('#username'),
+    "Alphanumeric characters only.",
+    function(input) {
+      usernameRegex = /^[a-z0-9]+$/i;
+      return input.match(usernameRegex);
+    }
+  );
+
+  var passwordHandler = createFormHandler(
+    $('#password'),
+    "Must be 8 characters or more.",
+    function(input) {
+      return input.length >= 8;
+    }
+  );
+
+  var emailHandler = createFormHandler(
+    $('#email'),
+    "Must have '@' character in it.",
+    function(input) {
+      return input.indexOf('@') !== -1;
+    }
+  );
+
+  $('#username').focus(userHandler);
+  $('#password').focus(passwordHandler);
+  $('#email').focus(emailHandler);
+  $('#username').blur(userHandler);
+  $('#password').blur(passwordHandler);
+  $('#email').blur(emailHandler);
 });
